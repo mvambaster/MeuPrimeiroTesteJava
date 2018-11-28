9b5ad5db6d9e164e75953d555834bbda133af8a5
mvambaster <michaelvambaster@gmail.com>
2018-11-28 14:50:58 -0200

    incluHelloWorld' e 'Greeter'

---
+++ HelloTeste.java
@@ -0,0 +1,8 @@
+/*Classe Teste*/
+public class HelloTeste{
+    public static void main(String[] args)
+    {
+        System.out.println("Voce acabou de compilar teste do Hello Java, Michael Vambaster");
+
+    }
+}
